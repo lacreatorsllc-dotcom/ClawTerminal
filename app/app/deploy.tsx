8b1eb29e7827c88bc2d5137464c2fd74f5f1b67d
Gabriel Kenworthy <mememarketer@Gabriels-MacBook-Air.local>
2026-04-13 12:33:30 -0600

    auto: checkpoint 2026-04-13 12:33

--- app/app/deploy.tsx
+++ app/app/deploy.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import {
   View, Text, TouchableOpacity, StyleSheet, ScrollView,
-  TextInput, ActivityIndicator, Linking, SafeAreaView,
+  TextInput, ActivityIndicator, Linking,
 } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import { useAuthStore } from '../stores/authStore'
